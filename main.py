bc258538dbdfc9ebea9057da2d312c81ab2992a7
Tr3wk <Bartlomiej.kacprzyk05@gmail.com>
2026-05-04 12:01:05 +0200

    Dodałem Opcje zapisu nowej aktywności do pliku .TXT

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("test")
+from datetime import datetime
+
+while True:
+    print("Witaj w Personal Tracker! co chcesz dzisiaj zrobić?")
+    print("1. Dodać nowy wpis")
+    print("2. Wyświetlić wszystkie wpisy")
+    print("3. Wyjść")
+    choice = input("Wybierz opcję: ")
+    if choice == "1":
+        Nazwa = input("Nazwa twojej aktywności (jedno słowo): ")
+        Opis = input("Opis twojej aktywności: ")
+        Czas = input("Czas trawnia aktywności: ")
+        data = datetime.now().strftime("%Y-%m-%d")
+        with open(f"{data}-{Nazwa}.txt", "a") as plik:
+            plik.write(data)
+            plik.write(f"\n{Nazwa}")
+            plik.write(f"\n{Czas}")
+            plik.write(f"\n{Opis}")
+
