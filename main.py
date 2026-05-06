2fd848b510a2589d84581f50f520045f7023bfa4
Tr3wk <Bartlomiej.kacprzyk05@gmail.com>
2026-05-07 00:08:24 +0200

    obsługa 2, 3, plus zmiana zapisu do plików JSON

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 
 while True:
     print("Witaj w Personal Tracker! co chcesz dzisiaj zrobić?")
@@ -7,13 +8,23 @@ while True:
     print("3. Wyjść")
     choice = input("Wybierz opcję: ")
     if choice == "1":
-        Nazwa = input("Nazwa twojej aktywności (jedno słowo): ")
-        Opis = input("Opis twojej aktywności: ")
-        Czas = input("Czas trawnia aktywności: ")
+        nazwa = input("Nazwa twojej aktywności (jedno słowo): ")
+        opis = input("Opis twojej aktywności: ")
+        czas = input("Czas trawnia aktywności: ")
         data = datetime.now().strftime("%Y-%m-%d")
-        with open(f"{data}-{Nazwa}.txt", "a") as plik:
+        with open("PersonalTracker/Tracker.txt", "a") as plik:
             plik.write(data)
-            plik.write(f"\n{Nazwa}")
-            plik.write(f"\n{Czas}")
-            plik.write(f"\n{Opis}")
-
+            plik.write(f"\n{nazwa}")
+            plik.write(f"\n{czas}")
+            plik.write(f"\n{opis}")
+            plik.write("\n---\n")
+    elif choice == "2":
+        try:
+            with open('PersonalTracker/Tracker.txt', "r") as plik:
+                print(plik.read())
+        except FileNotFoundError:
+            print("Brak wpisów, zacznij od dodania pierwszego wpisu")
+    elif choice == "3":
+        exit()
+    else:
+        print("Prosze wybrać opcje 1, 2 lub 3")
