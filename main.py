d6a2ba2e857257b0aaefc8274ba1ab51b77f5ae1
Tr3wk <Bartlomiej.kacprzyk05@gmail.com>
2026-05-11 21:35:05 +0200

    Zmiana aplikacji w WebApp

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
-from Tracker import *
-from testy2 import *
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+from datetime import datetime
 
-inicjalizacja_bazy()
-while True:
+app = Flask(__name__)
 
-    print("Witaj w Personal Tracker! co chcesz dzisiaj zrobić?")
-    print("1. Dodać nowy wpis")
-    print("2. Wyświetlić wszystkie wpisy")
-    print("3. Usuń wpis")
-    print("4. Wyjść")
-    choice = input("Wybierz opcję: ")
-    if choice == "1":
-        dodaj_wpis()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    baza = sqlite3.connect('PersonalTracker/tracker.db')
+    cursor = baza.cursor()
+
+    if request.method == 'POST':
+        data = datetime.now().strftime("%Y-%m-%d")
+        nazwa = request.form['nazwa']
+        opis = request.form['opis']
+        czas = request.form['czas']
+        dane = (nazwa, opis, czas, data)
+        cursor.execute("INSERT INTO wpisy (nazwa, opis, czas, data) VALUES (?, ?, ?, ?)", dane)
+        baza.commit()
+        return redirect(url_for('index'))
         
-    elif choice == "2":
-        odczyt_wpisow()
 
-    elif choice == "3":
-        usuniecie_wpisu()
+    cursor.execute("SELECT nazwa, opis, czas, data FROM wpisy")
+    wpisy = cursor.fetchall()
+    baza.close()
+    
 
-    elif choice == "4":
-        exit()
-    else:
-        print("Prosze wybrać opcje 1, 2, 3 lub 4")
+    return render_template('index.html', wpisy = wpisy )
+
+@app.route('/usun', methods=['POST'])
+def usun():
+    baza = sqlite3.connect('PersonalTracker/tracker.db')
+    cursor = baza.cursor()
+    nazwa_usun = request.form['nazwa_usun']
+    cursor.execute("DELETE FROM wpisy WHERE nazwa = ?", (nazwa_usun,))
+    baza.commit()
+    baza.close()
+
+
+    return redirect(url_for('index'))
+if __name__ == '__main__':
+    app.run(debug=True)
